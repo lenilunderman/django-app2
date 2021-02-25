486b04dfc8b245412f06d3dfb7e73d1a55cabff4
leni lunderman <lenilunderman@lenis-MacBook-Air.local>
2021-02-24 22:27:16 -0500

    test res

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -4,7 +4,7 @@ import os
 import sys
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'..
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
